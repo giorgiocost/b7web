958a59ae4b1ba8e3e4c1f0c48b609b3eb934b43f
giorgiocost <gegiorgio1991@gmail.com>
2018-10-05 18:12:04 -0300

    Efeito Fade

---
+++ jQuery/version2/src/assets/js/efeitoFade.js
@@ -0,0 +1,17 @@
+$(function() {
+    // $('#controlador').on('click',function () {
+    //     $('.quadrado').fadeOut();
+    // })
+
+    // $('#controlador').on('click',function () {
+    //     $('.quadrado').fadeIn();
+    // })
+
+    // $('#controlador').on('click',function () {
+    //     $('.quadrado').fadeTo('slow', 0.1);
+    // })
+
+    $('#controlador').on('click',function () {
+        $('.quadrado').fadeToggle('slow');
+    })
+})
